df43130efe6e9d9afc20e997625d8d38068f9771
Cheh91 <4eh91@ukr.net>
2021-03-14 12:39:32 +0200

    added edit

--- assets/src/js/my.js
+++ assets/src/js/my.js
@@ -52,9 +52,7 @@ function scrollFunction() {
 
     if ($(window).width() >= 1280) {
 
-      var phrase1 = Math.max(340 - 1 * window.scrollY) + "px";
-
-      
+      $(".phrase-one").css("left", Math.max(340 - 1 * window.scrollY) + "px");
       $(".phrase-two").css("left", Math.max(1500 - 1 * window.scrollY) + "px");
       $(".phrase-three").css("left", Math.max(3600 - 1 * window.scrollY) + "px");
       $(".phrase-four").css("left", Math.max(5400 - 1 * window.scrollY) + "px");
@@ -62,24 +60,21 @@ function scrollFunction() {
       $(".clients__block").css("right", Math.max(6700 - 1 * window.scrollY) + "px");
     } else if ($(window).width() > 768 && $(window).width() < 1279) {
      
-      var phrase1 = Math.max(150 - 0.5 * window.scrollY) + "px";
+      $(".phrase-one").css("left", Math.max(150 - 0.5 * window.scrollY) + "px")
       $(".phrase-two").css("left", Math.max(800 - 0.5 * window.scrollY) + "px");
       $(".phrase-three").css("left", Math.max(2000 - 0.7 * window.scrollY) + "px");
       $(".phrase-four").css("left", Math.max(3300 - 0.7 * window.scrollY) + "px");
       $(".phrase-five").css("left", Math.max(6200 - 1 * window.scrollY) + "px");
       $(".clients__block").css("right", Math.max(6300 - 1 * window.scrollY) + "px");
     } else if ($(window).width() < 768) {
-      var phrase1 = Math.max(50 - 0.7 * window.scrollY) + "px";
     
+      $(".phrase-one").css("left", Math.max(50 - 0.7 * window.scrollY) + "px");
       $(".phrase-two").css("left", Math.max(550 - 0.5 * window.scrollY) + "px");
-      $(".phrase-three").css("left", Math.max(2500 - 0.7 * window.scrollY) + "px");
-      $(".phrase-four").css("left", Math.max(4700 - 0.9 * window.scrollY) + "px");
+      $(".phrase-three").css("left", Math.max(2100 - 0.7 * window.scrollY) + "px");
+      $(".phrase-four").css("left", Math.max(4400 - 0.9 * window.scrollY) + "px");
       $(".phrase-five").css("left", Math.max(8500 - 1.2 * window.scrollY) + "px");
       $(".clients__block").css("right", Math.max(8300 - 1.2 * window.scrollY) + "px");
     }
-
-    console.log(phrase1);
-    $(".phrase-one").css("left", phrase1);
   })
 
 }
